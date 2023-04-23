cb5b39f3307e744068cfb96bd04e9027ba393da2
Ajay Binod <ajaybinod27@gmail.com>
2023-04-24 01:17:05 +0530

    questions viewset done

--- Mini-Project/core/technicalquestions_api/api/urls.py
+++ Mini-Project/core/technicalquestions_api/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from technicalquestions_api.api.views import ResultsViewSet
+from technicalquestions_api.api.views import ResultsViewSet,QuizQuestionViewSet
 
 router = routers.DefaultRouter()
 router.register(r'results', ResultsViewSet)
+router.register(r'questions', QuizQuestionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- Mini-Project/core/technicalquestions_api/api/views.py
+++ Mini-Project/core/technicalquestions_api/api/views.py
@@ -2,10 +2,15 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from technicalquestions_api.models import Results
-from technicalquestions_api.api.serializers import ResultsSerializer
+from technicalquestions_api.models import Results,QuizQuestion
+from technicalquestions_api.api.serializers import ResultsSerializer,QuizQuestionSerializer
 
 class ResultsViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated,IsAdminUser]
     serializer_class = ResultsSerializer
     queryset = Results.objects.all()
+    
+class QuizQuestionViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated,IsAdminUser]
+    serializer_class = QuizQuestionSerializer
+    queryset = QuizQuestion.objects.all()    
